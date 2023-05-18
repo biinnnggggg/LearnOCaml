7878eb44e52d55fc2d76b08754b8e53a6fed0657
biinnnggggg <ning.chin03@gmail.com>
2023-05-18 12:44:24 +0800

    commit

--- tail_of_a_list.ml
+++ tail_of_a_list.ml
@@ -1,9 +1,13 @@
 (* Tail of a List *)
+
 let rec last alist =
   match alist with
-  | [] -> None (* if the list is empty then there is no last element *)
-  | [a] -> Some a (* if the last element it reached return *)
-  | a::tl -> last tl;; (* recurse on the tail of the list *)
+  | [] -> None
+  | [a] -> Some a
+  | a::tl -> last tl;;
+
 
 (* Testcases *)
+last [];;
+last ["a" ; "b"];;
 last ["a" ; "b" ; "c"];;
